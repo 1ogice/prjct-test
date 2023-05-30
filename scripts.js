867f4e0061694c2cc22f469df93e3d2bd4ce9f81
Parnak Ruslan <radvancep2@gmail.com>
2023-05-30 23:36:06 +0300

    add search form

--- scripts.js
+++ scripts.js
@@ -1,3 +1,17 @@
 function goBack() {
   history.back();
 }
+
+const input = document.querySelector("#artistInput");
+function checkMaxLenght() {
+  const maxValue = 5;
+
+  if (input.value.length > maxValue) {
+    input.style.borderColor = "red";
+    console.log(true);
+  } else {
+    input.style.borderColor = "#edeef0";
+    console.log(false);
+  }
+}
+input.addEventListener("input", checkMaxLenght);
